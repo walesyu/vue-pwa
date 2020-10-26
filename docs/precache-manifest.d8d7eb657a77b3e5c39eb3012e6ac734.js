8084b6ef9e1e2eddf9c814f1f7922a1e71c1fe16
wilson <wales520@gmail.com>
2020-10-26 23:00:25 +0800

    first build

---
+++ docs/precache-manifest.d8d7eb657a77b3e5c39eb3012e6ac734.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a69134fe134aae2da474",
+    "url": "/vue-pwa/css/app.ed9fe969.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue-pwa/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "933d2147dd75bacb4b2b3a229607df34",
+    "url": "/vue-pwa/index.html"
+  },
+  {
+    "revision": "169f4234d53bd05a2f7b",
+    "url": "/vue-pwa/js/about.b794e219.js"
+  },
+  {
+    "revision": "a69134fe134aae2da474",
+    "url": "/vue-pwa/js/app.e1488528.js"
+  },
+  {
+    "revision": "b1954e12767d089bf4d9",
+    "url": "/vue-pwa/js/chunk-vendors.2fdd0015.js"
+  },
+  {
+    "revision": "60c085f113db3516ba9ca7218306e94d",
+    "url": "/vue-pwa/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-pwa/robots.txt"
+  }
+]);
